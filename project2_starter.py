4db1863268d6fd5ba905ad2c1aa36ab693280269
Yeonwoo Shim <yeonwooshim@Yeonwooui-MacBookAir.local>
2026-04-02 16:08:27 -0400

    Implement avg_location_rating_by_room_type

--- project2_starter.py
+++ project2_starter.py
@@ -242,7 +242,20 @@ def avg_location_rating_by_room_type(data) -> dict:
     # ==============================
     # YOUR CODE STARTS HERE
     # ==============================
-    pass
+    totals = {}
+    counts = {}
+    for row in data:
+        room_type = row[5]
+        location_rating = row[6]
+        if location_rating == 0.0:
+            continue
+        totals[room_type] = totals.get(room_type, 0.0) + location_rating
+        counts[room_type] = counts.get(room_type, 0) +1
+
+    averages = {}
+    for room_type in totals:
+        averages[room_type] = round(totals[room_type] / counts[room_type], 1)
+    return averages
     # ==============================
     # YOUR CODE ENDS HERE
     # ==============================
